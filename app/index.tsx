c253b0ea63d1ce215d3964fc0b6ebfe6fe532114
Brayan David <brayandavid@Brayans-Mac-mini.local>
2025-05-29 20:42:08 -0400

    1.0

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
-import MaskedView from '@react-native-masked-view/masked-view';
-import { LinearGradient } from 'expo-linear-gradient';
-import GradientText from './GradientText';
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function SignupScreen() {
   return (
-    <ScrollView 
+    <ScrollView
       style={styles.container}
       contentContainerStyle={styles.scrollContent}
       bounces={false}
@@ -15,20 +20,19 @@ export default function SignupScreen() {
     >
       <View style={styles.content}>
         <View style={styles.header}>
-           {/* <Text style={styles.title}>
-             Welcome to{'\n'}Zoopixie
-            </Text> */}
+          <Text style={styles.title}>Welcome to</Text>
+          <Text style={styles.title}>Zoopixie</Text>
 
-            <GradientText text="Welcome to Zoopixie" style={styles.title} colors={['#f48d78', '#fdd161']} />
-              
-          <Text style={styles.subtitle}>
-            We help children bring their pictures to life
+          <Text style={[styles.subtitle, styles.subtitleFirstLine]}>
+            We help children bring
           </Text>
+
+          <Text style={styles.subtitle}>their pictures to life</Text>
         </View>
 
         <View style={styles.imageContainer}>
           <Image
-            source={require('../assets/images/child.png')}
+            source={require("../assets/images/child.png")}
             style={styles.childImage}
             resizeMode="contain"
           />
@@ -36,15 +40,15 @@ export default function SignupScreen() {
 
         <View style={styles.footer}>
           <Image
-            source={require('../assets/images/bg-accent-1.png')}
+            source={require("../assets/images/bg-accent-1.png")}
             style={styles.bgAccent}
             resizeMode="cover"
           />
-          
+
           <View style={styles.buttonContainer}>
             <TouchableOpacity style={styles.buttonWrapper}>
               <LinearGradient
-                colors={['#55b7fa', '#55f2a6']}
+                colors={["#55b7fa", "#55f2a6"]}
                 start={{ x: 0, y: 0 }}
                 end={{ x: 1, y: 0 }}
                 style={styles.getStartedButton}
@@ -52,14 +56,16 @@ export default function SignupScreen() {
                 <Text style={styles.getStartedText}>Get started</Text>
               </LinearGradient>
             </TouchableOpacity>
-            
+
             <TouchableOpacity style={styles.accountButton}>
               <Text style={styles.accountText}>I have an account</Text>
             </TouchableOpacity>
           </View>
 
           <Text style={styles.privacyText}>
-            <Text style={styles.privacyRegular}>By continuing you accept our: </Text>
+            <Text style={styles.privacyRegular}>
+              By continuing you accept our:{" "}
+            </Text>
             <Text style={styles.linkText}>Privacy Policy</Text>
             <Text style={styles.privacyRegular}>, </Text>
             <Text style={styles.linkText}>Terms of Use</Text>
@@ -75,7 +81,7 @@ export default function SignupScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff2dd',
+    backgroundColor: "#fff2dd",
   },
   scrollContent: {
     flexGrow: 1,
@@ -83,45 +89,50 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     padding: 20,
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
   },
   header: {
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 72,
     gap: 24,
   },
   title: {
     fontSize: 40,
-    textAlign: 'center',
-    fontFamily: 'BalooTammudu2-Bold', // or your custom font
-    lineHeight: 44,
-    fontWeight: 'bold',
-    color: '#7ebce8'
+    textAlign: "center",
+    fontFamily: "BalooTammudu2-Bold",
+    color: "#f8a96e",
+    marginBottom: -64,
+  },
+  titleSecondLine: {
+    marginTop: 10,
   },
   subtitle: {
     fontSize: 18,
-    textAlign: 'center',
-    color: '#585858',
-    fontFamily: 'BalooTammudu2-Bold',
-    maxWidth: 191,
-    lineHeight: 22,  
+    textAlign: "center",
+    color: "#585858",
+    fontFamily: "BalooTammudu2-Bold",
+    marginBottom: -39,
+    // lineHeight: 22,
+  },
+  subtitleFirstLine: {
+    marginTop: 20,
   },
   imageContainer: {
-    alignItems: 'center',
-    marginVertical: 40,
+    alignItems: "center",
+    marginVertical: 80,
   },
   childImage: {
     width: 320,
     height: 250,
   },
   footer: {
-    alignItems: 'center',
-    marginTop: 'auto',
-    position: 'relative',
+    alignItems: "center",
+    marginTop: "auto",
+    position: "relative",
     marginBottom: 40,
   },
   bgAccent: {
-    position: 'absolute',
+    position: "absolute",
     width: 230,
     height: 210,
     left: -118,
@@ -132,44 +143,44 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   buttonWrapper: {
-    width: '100%',
+    width: "100%",
   },
   getStartedButton: {
-    width: '100%',
+    width: "100%",
     borderRadius: 50,
     marginBottom: 10,
   },
   getStartedText: {
     fontSize: 24,
     marginTop: 8,
-    color: 'white',
-    textAlign: 'center',
-    fontFamily: 'BalooTammudu2-Bold',
+    color: "white",
+    textAlign: "center",
+    fontFamily: "BalooTammudu2-Bold",
   },
   accountButton: {
-    width: '100%',
+    width: "100%",
     borderRadius: 100,
     paddingVertical: 8,
-    alignItems: 'center',
+    alignItems: "center",
   },
   accountText: {
     fontSize: 20,
-    color: '#585858',
-    fontFamily: 'BalooTammudu2-Bold',
+    color: "#585858",
+    fontFamily: "BalooTammudu2-Bold",
   },
   privacyText: {
-    textAlign: 'center',
+    textAlign: "center",
     marginHorizontal: 20,
-    lineHeight: 15,
+
   },
   privacyRegular: {
     fontSize: 12,
-    color: '#585858',
-    fontFamily: 'BalooTammudu2-Bold',
+    color: "#585858",
+    fontFamily: "BalooTammudu2-Bold",
   },
   linkText: {
     fontSize: 12,
-    color: '#55b7fa',
-    fontFamily: 'BalooTammudu2-Bold',
+    color: "#55b7fa",
+    fontFamily: "BalooTammudu2-Bold",
   },
 });
